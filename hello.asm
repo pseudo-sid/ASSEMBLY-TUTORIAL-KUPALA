3098d536b07b8914a6a8f7d6e94f43c0f059151d
Siddharth Singh <siddharth19111@iiitd.ac.in>
2020-09-17 06:57:40 +0530

    taking input and printing output

--- hello.asm
+++ hello.asm
@@ -1,14 +1,48 @@
 section  .data
-         text db "Hello, World!", 10
+         text1 db "What is your name? "
+         text2 db "\nHello, "
+
+section  .bss
+         name resb 16
+
 section  .text
          global _start
 _start:
-         mov rax, 1
-         mov rdi, 1
-         mov rsi, text
-         mov rdx, 14
-         syscall
+         call _printtext1
+         call _getName
+         call _printtext2
+         call _printname
 
          mov rax, 60
          mov rdi, 0
          syscall
+
+_getName:
+         mov rax, 0
+         mov rdi, 0
+         mov rsi, name
+         mov rdx, 16
+         syscall
+         ret
+         
+_printtext1:
+         mov rax, 1
+         mov rdi, 1
+         mov rsi, text1
+         mov rdx, 20
+         syscall
+         ret
+_printtext2:
+         mov rax, 1
+         mov rdi, 1
+         mov rsi, text2
+         mov rdx, 7
+         syscall
+         ret
+_printname:
+         mov rax, 1
+         mov rdi, 1
+         mov rsi, name
+         mov rdx, 16
+         syscall
+         ret
